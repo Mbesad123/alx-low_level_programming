6dfa340abe243789cbd01ed11d31642238a1ebed
Mbesad123 <mbesad6@gmail.com>
2023-09-14 01:00:28 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,22 +7,20 @@
 
 int main(void)
 {
-	int i = 0;
-	long j = 1, k = 2;
+	unsigned long int term1 = 1;
+	unsigned long int term2 = 2;
+	unsigned long int next;
+	int i;
 
-	while (i < 50)
+	printf("%lu, ", term1);
+	for (i = 0 ; i < 50 ; i++)
 	{
-	if (i == 0)
-	printf("%ld", j);
-	else if (i == 1)
-	printf(", %ld", k);
-	else
-	{
-	k += j;
-	j = k - j;
-	printf(", %ld", k);
-	}
-	++i;
+		printf("%lu", term2);
+		next = term1 + term2;
+		term1 = term2;
+		term2 = next;
+		if (i != 49)
+			printf(", ");
 	}
 	printf("\n");
 	return (0);
